965ce619953fc152c0002dfdb61bcb4528260f43
Полина Орлова <cantatcaelum@yandex.ru>
2024-04-13 13:31:31 +0300

    fix: правки

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller","impl","mapper", "model", "service"})
+@SpringBootApplication(scanBasePackages = {"controller","impl","mapper", "model", "service", "service.impl"})
 public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
