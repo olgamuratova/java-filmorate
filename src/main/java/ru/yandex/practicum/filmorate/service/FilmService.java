837c9012e59ba92eb7c8cdc753be44c2c13353ae
Di-Mark <dima.markin228@yandex.ru>
2024-04-17 17:30:17 +0300

    feat: add-most-popular

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.db.FilmStorage;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,14 +30,52 @@ public class FilmService {
         filmStorage.deleteLike(filmId, userId);
     }
 
-    public List<Film> getPopularFilms(Integer count) {
-        return filmStorage.getFilms().stream()
+    public List<Film> getPopularFilms(Integer count,Integer genreId,Integer year) {
+        List<Film> result = filmStorage.getFilms().stream()
                 .sorted(Comparator.comparingInt(filmStorage::getLikesQuantity).reversed())
                 .limit(count)
                 .collect(Collectors.toList());
+        List<Film> finalResult = new ArrayList<>();
+        if (genreId == null && year == null){
+            return result;
+        } else if (genreId != null && year != null) {
+            Integer temp = 0;
+            for (Film film : result){
+                if (film.getReleaseDate().getYear() == year && getStatus(film,genreId) && temp < count){
+                    finalResult.add(film);
+                    temp++;
+                }
+            }
+        } else if (genreId == null){
+            Integer temp = 0;
+            for (Film film : result){
+                if (film.getReleaseDate().getYear() == year && temp < count){
+                    finalResult.add(film);
+                    temp++;
+                }
+            }
+        } else if (year == null) {
+            Integer temp = 0;
+            for (Film film : result){
+                if (getStatus(film,genreId) && temp < count){
+                    finalResult.add(film);
+                    temp++;
+                }
+            }
+        }
+        return result;
     }
 
     public List<Film> getFilmsByQuery(String query, String type) {
         return filmStorage.getFilmsByQuery(query, type);
+    }
+
+    private Boolean getStatus(Film film,Integer genreId){
+        for (Genre genre : film.getGenres()){
+            if (genre.getId() == genreId){
+                return true;
+            }
+        }
+        return false;
     }
 }
