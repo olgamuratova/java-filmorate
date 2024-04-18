2bb7050df82ff4b3f34fea6fb39a5f35ebb733a7
Di-Mark <dima.markin228@yandex.ru>
2024-04-18 16:43:14 +0300

    fix: функционал

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -41,38 +41,19 @@ public class FilmService {
     }
 
     public List<Film> getPopularFilms(Integer count, Integer genreId, Integer year) {
-        List<Film> result = filmStorage.getFilms().stream()
+        return filmStorage.getFilms().stream()
                 .sorted(Comparator.comparingInt(filmStorage::getLikesQuantity).reversed())
+                .filter(film -> (genreId == null || hasGenre(film, genreId))
+                        && (year == null || film.getReleaseDate().getYear() == year))
                 .limit(count)
                 .collect(Collectors.toList());
-        if (genreId != null && year != null) {
-            return filmStorage.getFilms().stream()
-                    .sorted(Comparator.comparingInt(filmStorage::getLikesQuantity).reversed())
-                    .filter(f -> f.getReleaseDate().getYear() == year && getStatus(f, genreId))
-                    .limit(count)
-                    .collect(Collectors.toList());
-        } else if (genreId == null) {
-            return filmStorage.getFilms().stream()
-                    .sorted(Comparator.comparingInt(filmStorage::getLikesQuantity).reversed())
-                    .filter(f -> f.getReleaseDate().getYear() == year)
-                    .limit(count)
-                    .collect(Collectors.toList());
-        } else if (year == null) {
-            return filmStorage.getFilms().stream()
-                    .sorted(Comparator.comparingInt(filmStorage::getLikesQuantity).reversed())
-                    .filter(f -> getStatus(f, genreId))
-                    .limit(count)
-                    .collect(Collectors.toList());
-        }
-        return result;
     }
-
 
     public List<Film> getFilmsByQuery(String query, String type) {
         return filmStorage.getFilmsByQuery(query, type);
     }
 
-    private Boolean getStatus(Film film, Integer genreId) {
+    private Boolean hasGenre(Film film, Integer genreId) {
         for (Genre genre : film.getGenres()) {
             if (genre.getId() == genreId) {
                 return true;
